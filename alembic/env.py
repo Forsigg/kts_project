0d6d1ae84667b5947951408a0df23d5d6f606960
forsigg <al.berhmiller@gmail.com>
2022-09-10 20:55:34 +0700

    Исправлено дублирование кода в alembic.ini, URL к базе формируется из файла config.yml

--- alembic/env.py
+++ alembic/env.py
@@ -1,24 +1,28 @@
 import asyncio
-from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
+import yaml
+
 from sqlalchemy.engine import Connection
-from sqlalchemy.ext.asyncio import AsyncEngine
+from sqlalchemy.ext.asyncio import create_async_engine
 
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 from app.admin.models import AdminModel
-from app.quiz.models import ThemeModel
+from app.web.config import DatabaseConfig
 
 config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-if config.config_file_name is not None:
-    fileConfig(config.config_file_name)
+config_path = '../config.yml.'
+with open(config_path, "r") as f:
+    raw_config = yaml.safe_load(f)
+
+config = DatabaseConfig(**raw_config['database'])
+db_url = f"postgresql+asyncpg://{config.user}:{config.password}@{config.host}:" \
+         f"{config.port}/{config.database}"
 
 # add your model's MetaData object here
 # for 'autogenerate' support
@@ -44,7 +48,7 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = config.get_main_option("sqlalchemy.url")
+    url = db_url
     context.configure(
         url=url,
         target_metadata=target_metadata,
@@ -70,14 +74,7 @@ async def run_migrations_online() -> None:
     and associate a connection with the context.
 
     """
-    connectable = AsyncEngine(
-        engine_from_config(
-            config.get_section(config.config_ini_section),
-            prefix="sqlalchemy.",
-            poolclass=pool.NullPool,
-            future=True,
-        )
-    )
+    connectable = create_async_engine(db_url, future=True)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
